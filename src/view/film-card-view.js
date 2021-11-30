3a1d0ac0c3fc1f6996ec4caf332fe732e2f0062e
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2021-11-30 15:38:12 +0700

    Implemented data filling

--- src/view/film-card-view.js
+++ src/view/film-card-view.js
@@ -1,21 +1,35 @@
-const createFilmCardTemplate = () => (
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+
+import { formatFilmDuration, formatReleaseYear, formatTotalRating, formatShortDescription } from '../utils/format.js';
+
+
+dayjs.extend(duration);
+
+
+const getControlActiveClass = (parameter) => (
+  parameter ? 'film-card__controls-item--active' : ''
+);
+
+
+const createFilmCardTemplate = (film) => (
   `<article class="film-card">
     <a class="film-card__link">
-      <h3 class="film-card__title">The Dance of Life</h3>
-      <p class="film-card__rating">8.3</p>
+      <h3 class="film-card__title">${film.title}</h3>
+      <p class="film-card__rating">${formatTotalRating(film.totalRating)}</p>
       <p class="film-card__info">
-        <span class="film-card__year">1929</span>
-        <span class="film-card__duration">1h 55m</span>
-        <span class="film-card__genre">Musical</span>
+        <span class="film-card__year">${formatReleaseYear(film.release.date)}</span>
+        <span class="film-card__duration">${formatFilmDuration(film)}</span>
+        <span class="film-card__genre">${film.genre[0]}</span>
       </p>
-      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-      <span class="film-card__comments">5 comments</span>
+      <img src="${film.poster}" alt="Poster of ${film.title}" class="film-card__poster">
+      <p class="film-card__description">${formatShortDescription(film.description)}</p>
+      <span class="film-card__comments">${film.comments.length} comments</span>
     </a>
     <div class="film-card__controls">
-      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
-      <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
-      <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
+      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist ${getControlActiveClass(film.userDetails.watchlist)}" type="button">Add to watchlist</button>
+      <button class="film-card__controls-item film-card__controls-item--mark-as-watched ${getControlActiveClass(film.userDetails.alreadyWatched)}" type="button">Mark as watched</button>
+      <button class="film-card__controls-item film-card__controls-item--favorite ${getControlActiveClass(film.userDetails.favorite)}" type="button">Mark as favorite</button>
     </div>
   </article>`
 );
--- src/view/profile-view.js
+++ src/view/profile-view.js
@@ -1,8 +1,48 @@
-const createProfileTemplate = () => (
+const UserRanks = {
+  NOVICE: {
+    RANK: 'Novice',
+    MIN_FILMS: 1,
+    MAX_FILMS: 10,
+  },
+  FAN: {
+    RANK: 'Fan',
+    MIN_FILMS: 11,
+    MAX_FILMS: 20,
+  },
+  MOVIE_BUFF: {
+    RANK: 'Movie Buff',
+    MIN_FILMS: 21,
+    MAX_FILMS: Infinity,
+  },
+};
+
+
+const getUserRankName = (watchedFilmsCount) => {
+  for (const userRank of Object.values(UserRanks)) {
+    if (watchedFilmsCount >= userRank.MIN_FILMS && watchedFilmsCount <= userRank.MAX_FILMS) {
+      return userRank.RANK;
+    }
+  }
+};
+
+
+const getUserRank = (films) => {
+  const watchedFilmsCount = films.reduce((count, film) => count + Number(film.userDetails.alreadyWatched), 0);
+
+  if (watchedFilmsCount === 0) {
+    return '';
+  }
+
+  return `<p class="profile__rating">${getUserRankName(watchedFilmsCount)}</p>`;
+};
+
+
+const createProfileTemplate = (films) => (
   `<section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
+    ${getUserRank(films)}
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>`
 );
+
 
 export { createProfileTemplate };
